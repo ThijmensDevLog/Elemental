6c49d4fb02ba880d9fdfcbfe1ad227c4713ae438
Thijmen <thijmen@thijmentiscool.com>
2026-02-21 12:11:47 +0100

    Initial commit

---
+++ src/main/java/com/thijmensdevlog/ElementalDataGenerator.java
@@ -0,0 +1,11 @@
+package com.thijmensdevlog;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ElementalDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
